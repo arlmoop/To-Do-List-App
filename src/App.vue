31ea7c7938407e81520a5ba86516632e6eafc81d
aurelien.kammerer <>
2026-01-26 16:26:48 +0100

    fix: add all the fix of the Nizar's review

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,40 @@
 <template>
-  <html class="h-full font-sans">
-    <body class="min-h-screen bg-background pt-7">
-      <h1 class="text-5xl font-extrabold text-center mb-8 text-gray-800 mb-10">📝 TO-DO LIST APP</h1>
+  <html class="h-full font-sans min-h-screen bg-background pt-7">
+    <h1 class="text-5xl font-extrabold text-center mb-8 text-gray-800 mb-10">
+      📝 TO-DO LIST APP
+    </h1>
 
-      <div class="task-builder ml-20 mb-10">
-        <input class="px-3 rounded-lg border-2 border-black focus:border-black focus:outline-none" v-model="newTask" placeholder="Add a new task"/>
-        <button class="px-3 bg-background_builder hover:bg-background_builder_hover" @click="addMission">Add task</button>
-      </div>
+    <div class="task-builder ml-20 mb-10">
+      <input
+        class="px-3 rounded-lg border-2 border-black focus:border-black focus:outline-none"
+        v-model="newTask"
+        placeholder="Add a new task"
+      />
+      <button
+        class="px-3 bg-background_builder hover:bg-background_builder_hover"
+        @click="addMission"
+      >
+        Add task
+      </button>
+    </div>
 
-      <ul class="list-disc pl-10">
-        <TaskItem
-          v-for="task in tasks" :key="task"
-          :mission=task.mission 
-          :done=task.done
-          @toggle-task="task.done = $event"
-          @delete-task="deleteTask(task.mission)" />
-      </ul>
-    </body>
+    <ul class="list-disc pl-10">
+      <TaskItem
+        v-for="(task, index) in tasks"
+        :key="index"
+        :title="task.title"
+        :isDone="task.isDone"
+        :id="task.id"
+        @toggle-task="task.isDone = $event"
+        @delete-task="deleteTask(task.id)"
+      />
+    </ul>
   </html>
 </template>
 
 <script setup>
-  import { ref } from 'vue'
-  import TaskItem from './TaskItem.vue'
+import { ref } from "vue";
+import { useTasks } from "./composables/useTasks.js";
 
-  const newTask = ref("")
-
-  const task = ref({
-    mission: "",
-    done: false
-  })
-  const tasks = ref([])
-
-  const addMission = () => {
-    if (newTask.value.length == 0) return
-
-    const addTask = {
-      mission: newTask.value,
-      done: false
-    }
-
-    tasks.value.push(addTask)
-    newTask.value = ""
-  }
-
-  const deleteTask = (mission) => {
-    const indexToDelete = tasks.value.findIndex(task => task.mission == mission)
-    tasks.value.splice(indexToDelete, 1)
-  }
+const { newTask, tasks, addMission, deleteTask } = useTasks();
 </script>
-
-<style scoped>
-  div.task-builder input {
-    height: 30px;
-    border-radius: 10px;
-    border: 2px solid black;
-    margin-right: 10px;
-  }
-
-  div.task-builder button {
-    height: 30px;
-    border-radius: 10px;
-    border: 2px solid black;
-  }
-</style>
---
+++ src/composables/useTasks.js
@@ -0,0 +1,33 @@
+import { ref } from "vue";
+import TaskItem from "./TaskItem.vue";
+
+export const useTasks = () => {
+  const newTask = ref("");
+
+  const tasks = ref([]);
+
+  const addMission = () => {
+    if (newTask.value.length === 0) return;
+
+    const addTask = {
+      title: newTask.value,
+      isDone: false,
+      id: Date.now(),
+    };
+
+    tasks.value.push(addTask);
+    newTask.value = "";
+  };
+
+  const deleteTask = (id) => {
+    const indexToDelete = tasks.value.findIndex((task) => task.id === id);
+    tasks.value.splice(indexToDelete, 1);
+  };
+
+  return {
+    newTask,
+    tasks,
+    addMission,
+    deleteTask,
+  };
+};
